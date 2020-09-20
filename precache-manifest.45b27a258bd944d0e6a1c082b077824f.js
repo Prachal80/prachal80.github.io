d4bed01d20037b0dbb1c1ce8bddd41c7c7e9ee6e
Prachal Patel <prachal@Prachals-MacBook-Pro.local>
2020-09-20 01:04:57 -0700

    Updates

---
+++ precache-manifest.45b27a258bd944d0e6a1c082b077824f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e534488b4970e62ec125c3f9b10707c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d37f64084644cdd5a556",
+    "url": "/static/css/main.f60eae6a.chunk.css"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/js/2.0789156f.chunk.js"
+  },
+  {
+    "revision": "b25e0c44d0b24745f7d62e2ea6be31e5",
+    "url": "/static/js/2.0789156f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d37f64084644cdd5a556",
+    "url": "/static/js/main.7a80c965.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
